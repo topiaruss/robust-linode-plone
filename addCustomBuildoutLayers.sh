fc5fe7c513d8401a38bebd9eb866e97e2501e0ed
Russ Ferriday <russf@topia.com>
2010-09-03 22:29:27 +0000

    moved plone specific stuff into a new repos

--- addCustomBuildoutLayers.sh
+++ addCustomBuildoutLayers.sh
@@ -3,13 +3,23 @@
 DEST=`python -c "import os;s=os.path.expanduser('~');print s"`
 DEST=$DEST/Plone4
 
-echo 'rename the original buildout.cfg'
-mv $DEST/zeocluster/buildout.cfg $DEST/zeocluster/unified-buildout.cfg
 
-echo 'add the new buildout layers'
-cp *.cfg $DEST/zeocluster
+if [ -f "${DEST}/unified-buildout.cfg" ]
+then
+  echo "skipping install of buildout layers"
+else
 
-echo 're-run buildout to add the new buildout layers'
-cd $DEST/zeocluster
-./bin/buildout -v 
+  echo 'rename the original buildout.cfg'
+  mv $DEST/zeocluster/buildout.cfg $DEST/zeocluster/unified-buildout.cfg
+
+  echo 'add the new buildout layers'
+  cp *.cfg $DEST/zeocluster
+
+  chown plone:plone *.cfg
+
+  echo 're-run buildout to add the new buildout layers'
+  cd $DEST/zeocluster
+  ./bin/buildouti
+
+fi 
 
---
+++ installRunlevelCommands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Russ Ferriday, Topia Systems Ltd, russf@topia.com
+# run as root
+# run within robust-linode-plone
+
+cp plone4 ../Plone4/zeocluster/bin
+chmod ug+x ../Plone4/zeocluster/bin/plone4
+ln -s ../Plone4/zeocluster/bin/plone4 /etc/init.d/plone4
+chmod o+x ../Plone4/zeocluster/bin/plone4
+/usr/sbin/update-rc.d -f plone4 defaults
+
+
