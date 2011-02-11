a2ad233619a959cd341db182ddc0033abe84a9ec
Russ Ferriday <russf@topia.com>
2011-02-10 21:32:55 -0800

    doUI.sh

--- doUI.sh
+++ doUI.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 #Does the universal install for plone4
-
+echo 'Entered doUI.sh - to run the Unified Installer'
 #This is the most volatile part of the install
 URL=http://launchpad.net/plone/4.0/4.0.3/+download/Plone-4.0.3-20110720-UnifiedInstaller.tgz
 
-echo 'getting the Universal Installer'
+echo 'getting the Universal Installer from '
+echo $URL
 cd scratch
 curl -OL $URL
 
